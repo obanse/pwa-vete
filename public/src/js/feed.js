44912290963de32fc16d1614007517b39455d314
obanse <oliver.banse@outlook.com>
2020-09-27 23:10:17 +0200

    - add responsive picture banner

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -71,8 +71,6 @@ function createCard(data) {
   const cardTitle = document.createElement('div');
   cardTitle.className = 'mdl-card__title';
   cardTitle.style.backgroundImage = 'url(' + data.image + ')';
-  cardTitle.style.backgroundSize = 'cover';
-  cardTitle.style.height = '180px';
   cardWrapper.append(cardTitle);
   const cardTitleTextElement = document.createElement('h2');
   cardTitleTextElement.style.color = 'white';
