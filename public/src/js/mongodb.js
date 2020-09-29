fb1ea419508ebc5e0af17c4b9c538954cb41e6cb
obanse <oliver.banse@outlook.com>
2020-09-29 22:27:17 +0200

    - add mongo db support an read all data

--- public/src/js/mongodb.js
+++ public/src/js/mongodb.js
@@ -7,16 +7,42 @@ const uri = 'mongodb+srv://'
     + process.env.MONGO_SRV + '/'
     + process.env.MONGO_DB + '?retryWrites=true&w=majority';
 console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true });
-
-client.connect(err => {
-    const collection = client.db("posts-store").collection("posts");
-    // perform actions on the collection object
-    if (collection) {
-        console.log('Connect DB', collection.collectionName);
-    } else {
-        console.log('No Connection', collection.collectionName);
-    }
-
-    client.close();
+const client = new MongoClient(uri, {
+    useNewUrlParser: true
 });
+
+function iterateFunc(doc) {
+    console.log(JSON.stringify(doc, null, 4));
+}
+
+function errorFunc(error) {
+    console.log(error);
+}
+
+function readData() {
+
+    client.connect(err => {
+        const db = client.db('posts-store');
+        let cursor = db.collection('posts').find({});
+        cursor.forEach(iterateFunc, errorFunc);
+
+        client.close();
+    });
+}
+
+function connectCollection() {
+
+    client.connect(err => {
+        const collection = client.db("posts-store").collection("posts");
+        // perform actions on the collection object
+        if (collection) {
+            console.log('Connect DB', collection.collectionName);
+        } else {
+            console.log('No Connection', collection.collectionName);
+        }
+
+        client.close();
+    });
+}
+
+readData();
