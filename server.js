ab64f0d382337d7672fc97be6ad56a9f59b66bdf
obanse <oliver.banse@outlook.com>
2020-09-29 21:24:11 +0200

    - add alternate port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 80;
 
 app.use(express.static('public'));
 
